830989c90ab0d05989eab5869c87123955415657
libor_oliva <libor.oliva@post.cz>
2024-09-02 23:43:35 +0200

    Add comments to config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,11 @@
-# Konfigurační soubor
-
+# URL adresa k souboru TXT, který obsahuje seznam PDF souborů k tisku
 TXT_URL = "https://raw.githubusercontent.com/liborgit/davmo-print-script/master/model_order_list/order_list.txt"
+
+# Základní URL adresa ke složce, kde jsou uloženy PDF soubory
 BASE_URL = "https://raw.githubusercontent.com/liborgit/davmo-print-script/master/model-pdf-files/"
+
+# Složka, kam se budou stahovat PDF soubory před tiskem
 DOWNLOAD_FOLDER = "downloaded-pdf-files"
+
+# Název souboru, ve kterém jsou zaznamenány již vytisknuté soubory
 LOG_FILE = "printed_files.txt"
