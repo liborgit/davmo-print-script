6dd64f98bda7e5d180d3fd19ab4d0b5bc9ebfed2
libor_oliva <libor.oliva@post.cz>
2024-09-03 21:59:09 +0200

    Rename model_order_list directory to model-order-list

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 # URL adresa k souboru TXT, který obsahuje seznam PDF souborů k tisku
-TXT_URL = "https://raw.githubusercontent.com/liborgit/davmo-print-script/master/model-order-list/order_list.txt"
+TXT_URL = "https://raw.githubusercontent.com/liborgit/davmo-print-script/master/model_order_list/order_list.txt"
 
 # Základní URL adresa ke složce, kde jsou uloženy PDF soubory
-BASE_URL = "https://raw.githubusercontent.com/liborgit/davmo-print-script/master/model-pdf-files/"
+BASE_URL = "https://raw.githubusercontent.com/liborgit/davmo-print-script/master/model_pdf_files/"
 
 # Složka, kam se budou stahovat PDF soubory před tiskem
-DOWNLOAD_FOLDER = "downloaded-pdf-files"
+DOWNLOAD_FOLDER = "downloaded_pdf_files"
 
 # Název souboru, ve kterém jsou zaznamenány již vytisknuté soubory
 LOG_FILE = "printed_files.txt"
